ca57b56c883fecaafe0ecd8cc31c040941297442
Dong-Han-Kim <dh32110@gmail.com>
2023-10-24 11:46:42 +0900

    수정

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      배포를 다시 했습니다.
+      배포를 진행합니다.
     </main>
   )
 }
