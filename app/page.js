bf5a3f7a87dba9987de15f70397ff06ab91bb063
Dong-Han-Kim <dh32110@gmail.com>
2023-10-24 11:42:27 +0900

    재배포

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>고양이</div>
+      배포를 다시 했습니다.
     </main>
   )
 }
