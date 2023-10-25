e16aa31b8b245139c8d3789ea3e460094b78ee25
Dong-Han-Kim <dh32110@gmail.com>
2023-10-25 09:18:01 +0900

    DB연결 및 이미지 업로드 로직

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,10 @@
-import Image from 'next/image'
+"use client"
+import Image from 'next/image';
 import { Feed } from './components/Feed'
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { collection, getDocs } from 'firebase/firestore';
+import { firestore } from '@/firebase';
 
 // 3개 페이지
 // 새로운 이미지를 올리는 화면
@@ -7,9 +12,32 @@ import { Feed } from './components/Feed'
 // my 내 정보화면
 
 export default function Home() {
+  const [contents, setContents] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+
+  const fetchFeeds = async () => {
+    const snapShot = await getDocs(collection(firestore, "feeds"));
+    const nextContents = [];
+    snapShot.forEach((doc) => nextContents.push(doc.data()));
+    setContents(nextContents);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Feed />
-    </main>
-  )
+    <>
+      <h1>MAIN</h1>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {contents.map((content) => (
+        <Feed key={content.id} content={content} />
+      ))}
+      </main>
+        <button className='fixed bottom-10 left-0 right-0 mx-auto w-16 h-16 bg-[url(https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/768px-Instagram-Icon.png)] bg-cover shadow-xl'
+        onClick={() => router.push("/new")}
+        ></button>
+    </>
+
+  );
 }
--- firebase.js
+++ firebase.js
@@ -1,17 +1,34 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.apiKey,
+//   authDomain: process.env.authDomain,
+//   projectId: process.env.projectId,
+//   storageBucket: 'gs://instagram-f6362.appspot.com',
+//   messagingSenderId: process.env.messagingSenderId,
+//   appId: process.env.appId
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey:'AIzaSyCymjWUUwuQK5bO2K7fdCR9MjrHTzXyV-E',
+  authDomain:'instagram-f6362.firebaseapp.com',
+  projectId:'instagram-f6362',
+  storageBucket:'instagram-f6362.appspot.com',
+  messagingSenderId:'926529838532',
+  appId:'1:926529838532:web:9b7dd1f7ab0bb988411891'
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// 대부분의 값을 저장하는 DB
+export const firestore = getFirestore(app);
+// 이미지만을 저장하기 위한 저장소
+export const storage = getStorage(app);
